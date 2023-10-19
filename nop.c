c6cd03f641310b99363147c5fb8d372d9f1a56b4
sonie <garachu.muthoni@gmail.com>
2023-10-19 14:16:17 -0700

    nopimplementation git push

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop -main entry.
+ * Description: Doesnt do anything
+ * @top: element at the top of the stack (head)
+ * @line_number: constant int value in the structure
+ * Return: void
+ **/
+void nop(stack_t **top, unsigned int line_number)
+{
+	(void) top;
+	(void) line_number;
+}
