b4bc8e50a471b326f299201dabe541cfab352104
sonie <garachu.muthoni@gmail.com>
2023-10-19 03:10:22 -0700

    stack_functions and error_handlers

--- error_handler1.c
+++ error_handler1.c
@@ -19,3 +19,33 @@ void pop_error(unsigned int line)
 	fprintf(stderr, "L%u: can't pop an empty stack\n", line);
 	exit(EXIT_FAILURE);
 }
+/**
+ * swap_error - print err msg if stack contains less than two elements
+ * @line: line
+ * Return: void
+ */
+void swap_error(unsigned int line)
+{
+	fprintf(stderr, "L%u: can't swap, stack too short\n", line);
+	exit(EXIT_FAILURE);
+}
+/**
+ * add_error - Print error if the stack contains less than two elements
+ * @line: line
+ * Return: void
+ */
+void add_error(unsigned int line)
+{
+	fprintf(stderr, "L%u: can't add, stack too short\n", line);
+	exit(EXIT_FAILURE);
+}
+/**
+ * sub_error - Print error if the stack contains less than two elements
+ * @line: line
+ * Return: void
+ */
+void sub_error(unsigned int line)
+{
+	fprintf(stderr, "L%u: can't sub, stack too short\n", line);
+	exit(EXIT_FAILURE);
+}
